e5b938cffcd345d582f94699a75ea70a59b5a14c
Noda Yojiro <139129693+Noda-Yojiro@users.noreply.github.com>
2023-08-06 23:25:09 +0000

    Add files via upload

---
+++ Music Player in JavaScript/js/music-list.js
@@ -0,0 +1,158 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "KANASHIBARI feat.ao - （日剧《unknown》主题曲）",
+    artist: "RADWIMPS 、 ao",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "大団円 feat.ZORN",
+    artist: "RADWIMPS 、 ZORN",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "人間ごっこ",
+    artist: "RADWIMPS",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "有心論 - RADWIMPS 4 ~おかずのごはん~",
+    artist: "RADWIMPS",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "SUMMER DAZE - SUMMER DAZE",
+    artist: "RADWIMPS",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "週刊少年ジャンプ - 人間開花",
+    artist: "RADWIMPS",
+    img: "music-6",
+    src: "music-6"
+  },
+  {
+    name: "夢番地 - RADWIMPS 4 ~おかずのごはん~",
+    artist: "RADWIMPS",
+    img: "music-7",
+    src: "music-7"
+  },
+  {
+    name: "正解 (18FES ver.) - ANTI ANTI GENERATION",
+    artist: "RADWIMPS",
+    img: "music-8",
+    src: "music-8"
+  },
+  {
+    name: "猫じゃらし (Orchestra ver.) - 猫じゃらし",
+    artist: "RADWIMPS",
+    img: "music-9",
+    src: "music-9"
+  },
+  {
+    name: "バイ・マイ・サイド - セツナレンサ",
+    artist: "RADWIMPS",
+    img: "music-10",
+    src: "music-10"
+  },
+  {
+    name: "いいんですか? - RADWIMPS 4 ~おかずのごはん~",
+    artist: "RADWIMPS",
+    img: "music-11",
+    src: "music-11"
+  },  
+  {
+    name: "夢灯籠 - 君の名は。",
+    artist: "RADWIMPS",
+    img: "music-12",
+    src: "music-12"
+  },
+  {
+    name: "前前前世 (movie ver.) - 君の名は。",
+    artist: "RADWIMPS",
+    img: "music-13",
+    src: "music-13"
+  },
+  {
+    name: "スパークル (movie ver.) - 君の名は。",
+    artist: "RADWIMPS",
+    img: "music-14",
+    src: "music-14"
+  },
+  {
+    name: "なんでもないや (movie edit) - 君の名は。",
+    artist: "RADWIMPS",
+    img: "music-15",
+    src: "music-15"
+  },
+  {
+    name: "祝祭 (Movie edit) - 天気の子",
+    artist: "RADWIMPS",
+    img: "music-16",
+    src: "music-16"
+  },
+  {
+    name: "愛にできることはまだあるかい (Movie edit) - 天気の子",
+    artist: "RADWIMPS",
+    img: "music-17",
+    src: "music-17"
+  },
+  {
+    name: "大丈夫 (Movie edit) - 天気の子",
+    artist: "RADWIMPS",
+    img: "music-18",
+    src: "music-18"
+  },
+  {
+    name: "グランドエスケープ (Movie edit) - 天気の子",
+    artist: "RADWIMPS",
+    img: "music-19",
+    src: "music-19"
+  },
+  {
+    name: "カナタハルカ - すずめの戸締まり",
+    artist: "RADWIMPS",
+    img: "music-20",
+    src: "music-20"
+  },
+  {
+    name: "すずめ feat.十明 - すずめの戸締まり",
+    artist: "RADWIMPS",
+    img: "music-21",
+    src: "music-21"
+  },
+  {
+    name: "Tamaki - すずめの戸締まり",
+    artist: "RADWIMPS",
+    img: "music-22",
+    src: "music-22"
+  },
+  {
+    name: "すずめの涙 - すずめの戸締まり",
+    artist: "RADWIMPS",
+    img: "music-23",
+    src: "music-23"
+  },
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
